2e619e7361d278a843b06d30e7e41362e457e751
Michał Czapliński <czaplinskim@icloud.com>
2023-08-11 22:41:30 +0200

    Loader implemented

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+
+
 gsap.registerPlugin(MotionPathPlugin);
 
 const cloudToMove = document.querySelector("#cloudToMove")
@@ -98,3 +105,196 @@ function animateGradientColors() {
 
 
 checkbox.addEventListener('change', animateGradientColors);
+
+
+// START ANIMATION
+
+const t3 = gsap.timeline()
+.to("#pupil2", {
+    duration: 0.5,
+    repeat: -1,
+    yoyo: true,
+    attr: {
+        r: 11.5
+    }
+})
+
+const tl = gsap.timeline()
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 100,100 30,100 Z" 
+}})
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 100,100 30,100 Z"  
+}}, "<")
+.to(["#pupil1", "#pupil2"], {
+duration: 0.3,
+delay: 0.2,
+repeat: 0,
+attr:{
+    cx:"70"  
+}})
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 0,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 90,50 30,100 Z"
+}}, "<")
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 80,150 30,100 Z"  
+}}, "<")
+.to(["#pupil1", "#pupil2"], {
+duration: 0.2,
+repeat: 0,
+attr:{
+    cy:"105", 
+    cx:"70"  
+}},1.5)
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 0,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 100,60 30,100 Z"
+}}, "<")
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 100,150 30,100 Z"  
+}}, "<")
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 100,100 30,100 Z" 
+}})
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 100,100 30,100 Z"  
+}}, "<")
+.to(["#pupil1", "#pupil2"], {
+duration: 0.3,
+delay: 0.2,
+repeat: 0,
+attr:{
+    cy:"110", 
+    cx:"100"   
+}})
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 100,100 30,100 Z" 
+}})
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 1,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 100,100 30,100 Z"  
+}}, "<")
+.to(["#pupil1", "#pupil2"], {
+duration: 0.3,
+delay: 0.2,
+repeat: 0,
+attr:{
+    cy:"100", 
+    cx:"100"   
+}})
+.to("#eye-closed-top", {
+duration: 0.15,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,20 170,100 C170,100 100,100 30,100 Z" 
+}})
+.to("#eye-closed-bottom", {
+duration: 0.15,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,180 170,100 C170,100 100,100 30,100 Z"  
+}}, "<")
+.to(["#pupil1", "#pupil2", "#pupil3", "#eye1" ], {
+duration: 0.2,
+repeat: 0,
+yoyo: true,
+opacity: 0
+})
+.to("#eye-closed-top", {
+duration: 0.2,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,100 170,100 C170,100 100,100 30,100 Z"
+}})
+.to("#eye-closed-bottom", {
+duration: 0.2,
+repeat: 0,
+yoyo: true,
+attr:{
+d:"M30,100 Q100,100 170,100 C170,100 100,100 30,100 Z" 
+}}, "<")
+.to("#eye3", {
+duration: 0.2,
+repeat: 0,
+attr:{
+d:"M30,100 Q100,100 170,100 M30,100 Q100,100 170,100"
+}}, "<")
+.to(".eye-container", {
+duration: 1.5,
+repeat: 0,
+css:{
+'-webkit-mask-image': 'radial-gradient(circle at center, transparent 1000px, black 1000px)',
+'mask-image': 'radial-gradient(circle at center, transparent 50px, black 51px)'
+}, 
+onComplete: function() {
+  const element = document.querySelector(".eye-container");
+  element.parentNode.removeChild(element);
+
+}})
+.to("body", {
+  repeat: 0,
+css:{
+'overflow-y': 'auto',
+}
+})
+
+
+
+const timeline2 = gsap.timeline()
+.to("#note1", {
+duration: 4,
+repeat: -1,
+x: 45,
+y: -35,
+width: 12
+
+})
+.to("#note2", {
+duration: 4,
+repeat: -1,
+x: 25,
+y: -35,
+width: 12
+}, ">")
+
+
+
